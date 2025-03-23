f7fe4ea64cd6ceca1b90a22e390cf0859b0becda
jaime chico loayza <jaimechico14@gmai.com>
2025-03-22 21:20:07 -0500

    add components

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ Route::get('/', function () {
     return view('template');
 });
 
+Route::view('/panel', 'panel.index')->name('panel');
 
 Route::get('/login', function () {
     return view('auth.login');
